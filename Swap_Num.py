089490d9697e89b5ad3c4d082fc4de6f3f428272
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2023-10-30 18:52:28 +0530

    Add files via upload

---
+++ Swap_Num.py
@@ -0,0 +1,31 @@
+# without using 3 variable
+a, b = 3, 4
+a, b = b, a
+print(a, b)
+
+# using 3 variable
+a, b = 3, 4
+temp = a
+a = b
+b = temp
+print(a, b)
+
+# using addition and subtraction
+a, b = 3, 4
+a = a+b  # a = 3+4 --> 7
+b = a-b  # b = 7-4 --> 3
+a = a-b  # a = 7-3 --> 4
+print(a, b)
+
+# using multiplication and division
+a, b = 3, 4
+a *= b      # a = 3 X 4 --> 12
+b = a//b    # b = 12 // 4 --> 3
+a = a//b    # a = 12 // 3 --> 4
+print(a, b)
+
+# output:
+# 4 3
+# 4 3
+# 4 3
+# 4 3
