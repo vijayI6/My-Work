4c32cc93cb96d888c4e00cf847d79dc911466ce9
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2024-10-15 20:36:42 +0530

    Fibbonaci.py

--- Fibbonaci.py
+++ Fibbonaci.py
@@ -1,12 +1,23 @@
+# Using Logic
 first, second = 1, 1
-for i in range(int(input('Enter length :'))):
+sizes = int(input())
+for i in range(sizes):
     print(first, end=' ')
     first, second = second, first + second
 
 
-# for i in range(5):
-#     print(first, end=' ')
-#     first, second = second, first + second
-#
+# Using Recursion
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+size = int(input("Enter"))
+for i in range(size):
+    print(fibonacci(i), end=" ")
+
 # output:
+# 5
+# 1 1 2 3 5
 # 1 1 2 3 5
