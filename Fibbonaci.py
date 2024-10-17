ab2324b34000ce13cc807a43bb5a335e98649144
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2024-10-17 19:15:23 +0530

    Fibbonaci.py

--- Fibbonaci.py
+++ Fibbonaci.py
@@ -9,7 +9,7 @@ for i in range(sizes):
 # Using Recursion
 def fibonacci(n):
     if n == 0 or n == 1:
-        return 1
+        return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
@@ -20,4 +20,4 @@ for i in range(size):
 # output:
 # 5
 # 1 1 2 3 5
-# 1 1 2 3 5
+# 0 1 2 3 5
